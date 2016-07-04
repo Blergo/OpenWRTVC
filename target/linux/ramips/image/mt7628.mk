14c35f21ab55b886a7e44d6cc55405133f7e641c
Blergo <beardedowen@gmail.com>
2016-07-04 22:22:01 +0100

    add vocore2

--- target/linux/ramips/image/mt7628.mk
+++ target/linux/ramips/image/mt7628.mk
@@ -5,13 +5,20 @@
 Image/Build/Profile/MIWIFI-NANO=$(call BuildFirmware/Default16M/$(1),$(1),miwifi-nano,MIWIFI-NANO)
 Image/Build/Profile/MT7628=$(call BuildFirmware/Default4M/$(1),$(1),mt7628,MT7628)
 Image/Build/Profile/WRTNODE2P=$(call BuildFirmware/Default16M/$(1),$(1),wrtnode2p,WRTNODE2P)
+Image/Build/Profile/VOCORE2=$(call BuildFirmware/Default16M/$(1),$(1),vocore2,VOCORE2)
 
 define Image/Build/Profile/Default
 	$(call Image/Build/Profile/MIWIFI-NANO,$(1))
 	$(call Image/Build/Profile/MT7628,$(1))
 	$(call Image/Build/Profile/WRTNODE2P,$(1))
+	$(call Image/Build/Profile/VOCORE2,$(1))
 endef
 
 define Image/Build/Profile/MiwifiNano
 	$(call Image/Build/Profile/MIWIFI-NANO,$(1))
 endef
+
+define Image/Build/Profile/VOCORE2
+	$(call Image/Build/Profile/VOCORE2,$(1))
+endef
+
